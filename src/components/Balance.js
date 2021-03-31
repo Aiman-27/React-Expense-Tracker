720f622c688d919519cc1af40d992a7c65bb5c2e
Aiman-27 <74717048+Aiman-27@users.noreply.github.com>
2021-03-31 06:42:39 -0700

    Update Balance.js

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+//import the Global State
+import { GlobalContext } from '../context/GlobalState';
 
 export const Balance = () => {
+
+    const { transactions } = useContext(GlobalContext);
+
+    const transactionAmounts = transactions.map(transaction => transaction.transactionAmount);
+        
+    const balance = transactionAmounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
     return (
         <div>
             <h4>Current Balance</h4>
