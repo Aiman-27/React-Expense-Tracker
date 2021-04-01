b5b5fb1e49b566ce5bfb34714ddfc7ad3e4c1377
Aiman-27 <74717048+Aiman-27@users.noreply.github.com>
2021-04-01 07:56:29 -0700

    Update Balance.js

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext } from 'react'
 
 //import the Global State
 import { GlobalContext } from '../context/GlobalState';
@@ -14,7 +14,7 @@ export const Balance = () => {
     return (
         <div>
             <h4>Current Balance</h4>
-            <h1>$0.00</h1>
+            <h1 id="balance">${balance}</h1>
         </div>
     )
 }
