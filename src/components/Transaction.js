7836d2915e95eec05168c23c91b4be30c049b22a
Aiman-27 <74717048+Aiman-27@users.noreply.github.com>
2021-04-01 07:56:34 -0700

    Update Transaction.js

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext } from 'react'
 
 //import the Global State
 import { GlobalContext } from '../context/GlobalState';
@@ -11,7 +11,7 @@ export const Transaction = ({ transaction}) => {
     const transactionType = transaction.transactionAmount > 0 ? 'plus' : 'minus';
 
     return (
-        <li className="plus">
+        <li className={transactionType}>
             {transaction.description}
             <span>{sign}${Math.abs(transaction.transactionAmount)}</span>
             <button className="delete-btn"
