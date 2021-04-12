2224451fa9d4057973ba29810c66bc69151f1b73
Aiman-27 <74717048+Aiman-27@users.noreply.github.com>
2021-04-12 14:50:43 +0500

    Update AddTransaction.js

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useState } from 'react';
 
 //import the Global State
 import { GlobalContext } from '../context/GlobalState';
@@ -7,7 +7,7 @@ import { GlobalContext } from '../context/GlobalState';
 export const AddTransaction = () => {
 
     const [description, setDescription] = useState('');
-    const [transactionAmount, setTransactionAmount] = useState(0);
+    const [transactionAmount, setTransactionAmount] = useState('');
 
     const { addTransaction } = useContext(GlobalContext);
 
@@ -27,18 +27,31 @@ export const AddTransaction = () => {
     return (
         <div>
             <h3>Add New Transaction</h3>
-            <form>
+            <form onSubmit={onSubmit}>
                 <div className="form-control">
                     <label htmlFor="description">
                         Description
                     </label>
-                    <input type="text" id="description" placeholder="Detail of transaction" value={description} onChange={(e) => setDescription(e.target.value) } placeholder="Detail of transaction"/>
+                    <input  type="text" 
+                            id="description" 
+                            placeholder="Detail of transaction" 
+                            value={description} 
+                            onChange={(e) => setDescription(e.target.value) } 
+                            required="required"
+                    />
                 </div>
                 <div className="form-control">
                     <label htmlFor="transactionamount">
                         Transaction Amount
                     </label>
-                    <input type="number" id="transactionamount" placeholder="Enter transaction amount" value={transactionAmount} onChange={(e) => setTransactionAmount(e.target.value) } placeholder="Dollar Value of Transaction"/>
+                    <input type="number" 
+                    id="transactionamount" 
+                    placeholder="Enter transaction amount" 
+                    value={transactionAmount} 
+                    onChange={(e) => setTransactionAmount(e.target.value) } 
+                    placeholder="Dollar Value of Transaction"
+                    required="required"
+                    />
                 </div>
                 <button className="btn">
                     Add Transaction
